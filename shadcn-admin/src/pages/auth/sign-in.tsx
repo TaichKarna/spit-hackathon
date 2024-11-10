9d6c35138027981ec2e9af184925e93c018b56e6
TaichKarna <taichikarna@gmail.com>
2024-11-10 12:54:57 +0530

    working one

--- shadcn-admin/src/pages/auth/sign-in.tsx
+++ shadcn-admin/src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { UserAuthForm } from './components/user-auth-form'
 import ViteLogo from '@/assets/vite.svg'
+import icon from '../../assets/WhatsApp Image 2024-11-09 at 17.21.08.jpeg'
 
 export default function SignIn() {
   return (
@@ -8,19 +9,9 @@ export default function SignIn() {
         <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
           <div className='absolute inset-0 bg-zinc-900' />
           <div className='relative z-20 flex items-center text-lg font-medium'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            Shadcn Admin
+          <img src={icon} alt="Logo" className="w-16 h-16" />
+
+            UrbanAire
           </div>
 
           <img
--- shadcn-admin/src/pages/auth/sign-up.tsx
+++ shadcn-admin/src/pages/auth/sign-up.tsx
@@ -1,26 +1,19 @@
 import { Card } from '@/components/ui/card'
 import { SignUpForm } from './components/sign-up-form'
 import { Link } from 'react-router-dom'
+import icon from '../../assets/WhatsApp Image 2024-11-09 at 17.21.08.jpeg'
 
 export default function SignUp() {
   return (
     <>
-      <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
+      <div className='container grid h-svh flex-col items-center justify-center bg-white lg:max-w-none lg:px-0'>
         <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
           <div className='mb-4 flex items-center justify-center'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            <h1 className='text-xl font-medium'>Shadcn Admin</h1>
+          <div className='mb-4 flex items-center justify-center'>
+          <img src={icon} alt="Logo" className="w-16 h-16" />
+
+            <h1 className='text-xl font-medium'>UrbainAire</h1>
+          </div>
           </div>
           <Card className='p-6'>
             <div className='mb-2 flex flex-col space-y-2 text-left'>
